362c4b43003795389eb3cb50504077f313d4aa47
Luoxy <1028900373@qq.com>
2021-01-13 11:39:07 +0800

    文本溢出显示...

--- lib/pages/subject/subject_lesson.dart
+++ lib/pages/subject/subject_lesson.dart
@@ -1,3 +1,4 @@
+import 'package:flutter/cupertino.dart';
 import 'package:flutter/material.dart';
 
 class MyLessonPage extends StatefulWidget {
@@ -7,22 +8,22 @@ class MyLessonPage extends StatefulWidget {
 
 class _MyLessonPageState extends State<MyLessonPage> {
   final List lessonList=[
-    {"id": 1, "title": "课程名称1", "task_txt": "0/1", "valid_time": "0"},
+    {"id": 1, "title": "课程名称1这是一个很长很长很长很长的名称", "task_txt": "0/1", "valid_time": "0"},
     {"id": 2, "title": "课程名称2", "task_txt": "1/1", "valid_time": "1"},
     {"id": 3, "title": "课程名称3", "task_txt": "1/10", "valid_time": "20"},
     {"id": 4, "title": "课程名称4", "task_txt": "0/11", "valid_time": "365"},
     {"id": 5, "title": "课程名称5", "task_txt": "0/11", "valid_time": "0"},
   ];
+  final int _pageSize = 5;  //数量/页
 
   @override
   Widget build(BuildContext context) {
     return Container(
-      padding: EdgeInsets.all(20),
-      // color: Colors.black12,
+      padding: EdgeInsets.symmetric(horizontal: 20),
       child: ListView.builder(
-        itemCount: 5,
+        itemCount: _pageSize,
         itemBuilder: (ctx, i){
-          return buildLessonItem(lessonList[i]);
+          return buildLessonItem(lessonList[i], i == _pageSize - 1);
         },
       ),
     );
@@ -30,11 +31,9 @@ class _MyLessonPageState extends State<MyLessonPage> {
 }
 
 //课程列表项
-Widget buildLessonItem(item){
-  print('--item--');
-  print(item);
+Widget buildLessonItem(item, is_last_child){
   return Container(
-    margin: EdgeInsets.only(bottom: 20),
+    margin: is_last_child ? EdgeInsets.symmetric(vertical: 20) : EdgeInsets.only(top: 20),
     child: ClipRRect(
       borderRadius: BorderRadius.circular(10),
       child: Column(
@@ -51,20 +50,36 @@ Widget buildLessonItem(item){
 //列表项-头部
 Widget buildHeader(item){
   return Container(
-    height: 40,
-    padding: EdgeInsets.symmetric(horizontal: 20),
-    color: Color(0xff9085e7),
     child: Row(
-      mainAxisAlignment: MainAxisAlignment.spaceBetween,
       children: <Widget>[
-        Text(
-            item['title'],
-            overflow: TextOverflow.ellipsis,
-            style: TextStyle(fontSize: 18, fontWeight: FontWeight.bold, color: Colors.white)
-        ),
-        Image.asset("assets/images/right.png", width: 20,)
+        Expanded(
+          child: Container(
+            height: 40,
+            padding: EdgeInsets.symmetric(horizontal: 20),
+            color: Color(0xff9085e7),
+            alignment: Alignment.topCenter,
+            child: Column(
+              mainAxisAlignment: MainAxisAlignment.center,
+              children: <Widget>[
+                Row(
+                  mainAxisAlignment: MainAxisAlignment.spaceBetween,
+                  children: <Widget>[
+                    Expanded(
+                      child: Text(
+                          item['title'],
+                          overflow: TextOverflow.ellipsis,
+                          style: TextStyle(fontSize: 18, fontWeight: FontWeight.bold, color: Colors.white)
+                      ),
+                    ),
+                    Image.asset("assets/images/right.png", width: 20,)
+                  ],
+                )
+              ],
+            )
+          ),
+        )
       ],
-    ),
+    )
   );
 }
 
